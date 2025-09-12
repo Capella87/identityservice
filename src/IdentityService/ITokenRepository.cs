454ca21e28d61f03a70e31d6755303bc8b260fbe
Capella87 <capella87@outlook.com>
2025-09-12 12:30:54 +0900

    Add a repository for database providers

---
+++ src/IdentityService/ITokenRepository.cs
@@ -0,0 +1,25 @@
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Data;
+
+public interface ITokenRepository
+{
+    public Task<Result<IToken?>> GetTokenAsync(string token);
+
+    public Task<Result<IEnumerable<IToken>?>> GetTokensByUserIdAsync(string userId);
+
+    public Task<Result> RevokeTokenAsync(IToken token);
+
+    public Task<Result> RevokeAllTokensByUserIdAsync(string userId);
+
+    public Task<Result> SaveTokenAsync(IToken token);
+
+    public Task<Result> DeleteTokenAsync(IToken token);
+
+    public Task<Result<T?>> GetUserByIdAsync<T>(string userId) where T : class;
+}
---
+++ src/Jwt/JwtTokenRepository.cs
@@ -0,0 +1,94 @@
+using FluentResults;
+using IdentityService.Jwt;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Data;
+
+public class JwtTokenRepository<TUser, UserKey> : ITokenRepository
+    where TUser : IdentityUser<UserKey>
+    where UserKey : IEquatable<UserKey>
+{
+    private readonly DbContext _dbContext;
+
+    public JwtTokenRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result> DeleteTokenAsync(IToken token)
+    {
+        _dbContext.Set<JwtRefreshTokenEntity<TUser, UserKey>>().Remove((token as JwtRefreshTokenEntity<TUser, UserKey>)!);
+        await _dbContext.SaveChangesAsync();
+
+        return Result.Ok();
+    }
+
+    public async Task<Result<IToken?>> GetTokenAsync(string token)
+    {
+        var result = await _dbContext.Set<JwtRefreshTokenEntity<TUser, UserKey>>()
+            .Include(e => e.User)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.Token == token);
+
+        return result ?? (Result<IToken?>)Result.Fail("Token not found");
+    }
+
+    public Task<Result<IEnumerable<IToken>?>> GetTokensByUserIdAsync(string userId)
+    {
+        // Find all tokens;
+        throw new NotImplementedException();
+    }
+
+    public async  Task<Result<T?>> GetUserByIdAsync<T>(string userId) where T : class
+    {
+        var result = await _dbContext.Set<T>().FindAsync(userId);
+
+        return result ?? (Result<T?>)Result.Fail("User not found");
+    }
+
+    public async Task<Result> RevokeAllTokensByUserIdAsync(string userId)
+    {
+        var results = await _dbContext.Set<JwtRefreshTokenEntity<TUser, UserKey>>()
+            .Where(t => t.UserId!.ToString() == userId)
+            .ToListAsync();
+        if (results.Count == 0)
+        {
+            return Result.Ok();
+        }
+
+        _dbContext.Set<JwtRefreshTokenEntity<TUser, UserKey>>().RemoveRange(results);
+        await _dbContext.SaveChangesAsync();
+
+        return Result.Ok();
+    }
+
+    public Task<Result> RevokeTokenAsync(IToken token)
+    {
+        var entity = token as JwtRefreshTokenEntity<TUser, UserKey>;
+        _dbContext.Set<JwtRefreshTokenEntity<TUser, UserKey>>().Remove(entity!);
+
+        return _dbContext.SaveChangesAsync().ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                return Result.Fail(t.Exception?.Message ?? "Failed to revoke token");
+            }
+            return Result.Ok();
+        });
+    }
+
+    public Task<Result> SaveTokenAsync(IToken token)
+    {
+        var entity = token as JwtRefreshTokenEntity<TUser, UserKey>;
+        _dbContext.Set<JwtRefreshTokenEntity<TUser, UserKey>>().Add(entity!);
+        return _dbContext.SaveChangesAsync().ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                return Result.Fail(t.Exception?.Message ?? "Failed to save token");
+            }
+            return Result.Ok();
+        });
+    }
+}
